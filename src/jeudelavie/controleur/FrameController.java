01f6e58fa2c3c51ba15a836d5eb99d5eae575cf6
CieuFr <vicdeo.leiglat@gmail.com>
2022-03-22 12:23:35 +0100

    completing frameController and frame view

--- src/jeudelavie/controleur/FrameController.java
+++ src/jeudelavie/controleur/FrameController.java
@@ -1,23 +1,94 @@
 package jeudelavie.controleur;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import jeudelavie.vue.BoardView;
 
 public class FrameController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private Pane boardPane;
 
-    private BoardView boardView;
+    @FXML
+    private ComboBox patternLoadingCombo;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ComboBox lonelinessDeathCombo;
+
+    @FXML
+    private ComboBox sufocationDeathCombo;
+
+    @FXML
+    private ComboBox minHealthCombo;
+
+
+    @FXML
+    private ComboBox maxHealthCombo;
+
+    @FXML
+    private TextField boardSizeTextField;
+
+    @FXML
+    private Button playPauseButton;
+
+    @FXML
+    protected void onPlayPauseButtonAction(){
+        System.out.println("todo play pause");
+        playPauseButton.setText("Pause");
     }
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    protected void onResetButtonAction(){
+        System.out.println("TODO reset ");
+    }
+
+    @FXML
+    private Button nextGenerationButton;
+
+    @FXML
+    protected void onNextGenerationButtonAction(){
+        System.out.println("TODO next generation ");
+    }
+
+    @FXML
+    private Button randomizeButton;
+
+    @FXML
+    protected void onRandomizeButtonAction(){
+        System.out.println("TODO randomize ");
+    }
+
+    @FXML
+    private Button quitButton;
+
+    @FXML
+    protected void onQuitButtonAction(){
+        System.exit(0);
+    }
+
+    @FXML
+    private Button loadButton;
+
+    @FXML
+    protected void onLoadButtonAction(){
+        System.out.println("todo");
+    }
+
+
+
+    public void initialize(){
+
+
+    }
+
+
+    private BoardView boardView;
 
     public void addBoardView(BoardView boardView) {
         this.boardView = boardView;
