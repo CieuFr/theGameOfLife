21129b93273b3fb5969812aa78bedbec0cb7ebce
IronJulo <jules.brss@gmail.com>
2022-03-27 20:58:46 +0200

    [ADDED] Compute service

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.event.ActionEvent;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,45 +7,54 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import jeudelavie.controleur.BoardController;
+import jeudelavie.controleur.FigureController;
 import jeudelavie.controleur.FrameController;
 import jeudelavie.miscellaneous.ComputeService;
-import jeudelavie.miscellaneous.GameTimeLine;
 import jeudelavie.model.FrameModel;
-
-import java.util.Arrays;
-import java.util.Locale;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         BoardController boardController = new BoardController(100);
+        FigureController figureController = new FigureController(10);
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/jeudelavie/vue/frame-view.fxml"));
         fxmlLoader.setController(new FrameController(new FrameModel()));
         AnchorPane root = fxmlLoader.load();
         FrameController frameController = fxmlLoader.getController();
         frameController.addBoardController(boardController);
-        frameController.bindNumberOfIterations();
+        frameController.addFigureController(figureController);
+        frameController.bindNumberOfIterations(); // TODO in addboardcontroller
         boardController.draw();
 
         primaryStage.setTitle("The Game Of Life");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
 
-        GameTimeLine gameTimeLine = new GameTimeLine(boardController, frameController, Duration.millis(100));
 
-        //test(null);
-
-
-        // TODO WTF JAVA ?!?!?!?!? 
-        frameController.getBoardController().getBoardModel().getPlayingProperty().addListener(listener -> {
-            System.out.println(gameTimeLine.getBooleanProperty());
+        ComputeService computeService = new ComputeService(boardController, frameController);
+        computeService.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent event) {
+                System.out.println("Successfully computed board");
+                boardController.getBoardModel().setBoard((int[][]) event.getSource().getValue());
+                boardController.getBoardModel().increaseIterations();
+                boardController.draw();
+            }
         });
-        //frameController.getFrameModel().getPlayingProperty().bindBidirectional(gameTimeLine.getBooleanProperty());
-        gameTimeLine.getBooleanProperty().bind(frameController.getBoardController().getBoardModel().getPlayingProperty().and(frameController.getBoardController().getBoardModel().getPlayingProperty()));
-    }
-    public void test(String toto){
-        System.out.println(toto.toUpperCase(Locale.ROOT));
+        computeService.setPeriod(Duration.millis(10)); // TODO slider
+
+        boardController.getBoardModel().getPlayingProperty().addListener(change -> {
+            if (boardController.getBoardModel().isPlaying()){
+                computeService.start();
+            }else {
+                computeService.cancel();
+                computeService.reset();
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
--- src/jeudelavie/miscellaneous/ComputeService.java
+++ src/jeudelavie/miscellaneous/ComputeService.java
@@ -1,30 +1,29 @@
 package jeudelavie.miscellaneous;
 
+import javafx.concurrent.ScheduledService;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import jeudelavie.controleur.BoardController;
 import jeudelavie.controleur.FrameController;
 
-public class ComputeService extends Service<int[][]> {
-    private BoardController boardController;
-    private FrameController frameController;
+public class ComputeService extends ScheduledService<int[][]> {
+    private final BoardController boardController;
+    private final FrameController frameController;
 
     public ComputeService(BoardController boardController, FrameController frameController) {
         super();
         this.boardController = boardController;
         this.frameController = frameController;
+
         this.setOnSucceeded(workerStateEvent -> {
             System.out.println("success");
             System.out.println(workerStateEvent);
-
-            //this.restart();
         });
-        //this.stateProperty().addListener(new ComputeListener(this, boardController));
     }
 
     @Override
     protected Task<int[][]> createTask() {
-        return new Task<int[][]>() {
+        return new Task<>() {
             @Override
             protected int[][] call() throws Exception {
                 System.out.println("computing");
